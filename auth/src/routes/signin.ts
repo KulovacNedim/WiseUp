f07cf9ba476cf84740e6d7ad1478241f081c88c6
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-10-12 22:53:42 +0200

    signing in and out and retrieving currently logged in user implemented. validation logic separeted in middlewares

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,46 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { BadRequestError } from '../errors/BadRequestError';
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { PasswordManager } from '../services/password-manager';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('HI there!');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('Password is required'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) throw new BadRequestError('Invalid credentials');
+
+    const passwordMatch = await PasswordManager.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordMatch) throw new BadRequestError('Invalid credentials');
+
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(201).send(existingUser);
+  }
+);
 
 export { router as signinRouter };
