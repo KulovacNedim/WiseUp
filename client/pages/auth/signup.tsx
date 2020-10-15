0d8e47c859ae4e32ebc12779b64193568e02a6c1
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-10-15 16:44:18 +0200

    client service converted to use typescript

---
+++ client/pages/auth/signup.tsx
@@ -0,0 +1,18 @@
+const Signup = () => {
+  return (
+    <form>
+      <h1>Sign Up</h1>
+      <div>
+        <label>Email Address</label>
+        <input />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" />
+      </div>
+      <button>Sign Up</button>
+    </form>
+  );
+};
+
+export default Signup;
