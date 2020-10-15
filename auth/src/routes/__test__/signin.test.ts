bfbda32e3b3b731d6dbcca854c09bb1ee6cf70cf
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-10-15 14:38:48 +0200

    testing auth service: test environtment and setup defined, tests for routers defined as well. index.ts and app.ts files refactored to better suit testing libraries

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,86 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('fails when a email that does not exist is supplied', async () => {
+  return request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('responds with a cookie when given valid credentials', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(200);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
+
+it('fails when an incorrect password is supplied', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: 'password-wrong',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid email', async () => {
+  const response = await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'not-email',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+      password: '',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email or password', async () => {
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      email: 'test@test.com',
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signin')
+    .send({
+      password: 'password',
+    })
+    .expect(400);
+});
