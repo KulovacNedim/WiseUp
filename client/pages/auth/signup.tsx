31a53fa310fda6e281d2b456c818c79434bf6bea
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-10-16 14:26:19 +0200

    tailwindcss configured to be used in project

--- client/pages/auth/signup.tsx
+++ client/pages/auth/signup.tsx
@@ -1,17 +1,57 @@
 const Signup = () => {
   return (
-    <form>
-      <h1>Sign Up</h1>
-      <div>
-        <label>Email Address</label>
-        <input />
+    <>
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+        <h1 className="text-3xl mb-5">Sign Up</h1>
+        <form>
+          <div className="mb-4">
+            <label
+              className="block text-gray-600 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-600"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-600 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-gray-600 mb-3"
+              id="password"
+              type="password"
+              placeholder="******************"
+            />
+            <p className="text-red-600 text-xs italic">
+              Please choose a password.
+            </p>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              type="button"
+            >
+              Sign In
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-700"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
       </div>
-      <div>
-        <label>Password</label>
-        <input type="password" />
-      </div>
-      <button>Sign Up</button>
-    </form>
+    </>
   );
 };
 
